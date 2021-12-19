04f50f1c0db565e3b848612de5e6b3eb7281a0b9
Gustavo Valle <gustavo.vallerp@hotmail.com>
2021-12-19 00:31:07 -0300

    initial configuration to a typescript project

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', async (req, res) => {
+    res.send('Hello World');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`> [server] Server is running at http://localhost:${PORT}`)
+})
