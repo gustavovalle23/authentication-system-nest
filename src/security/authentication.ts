5bd0ec7c0b746dc32dae9eb4d80a306d7880197f
Gustavo Valle <gustavo.vallerp@hotmail.com>
2021-12-19 18:18:50 -0300

    Typed error and send information to client

--- src/security/authentication.ts
+++ src/security/authentication.ts
@@ -1,6 +1,5 @@
 import { Response, Request, NextFunction } from 'express-serve-static-core';
-
-import jwt from 'jsonwebtoken';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
 
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
@@ -8,12 +7,11 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) return res.sendStatus(401);
     const secretKey = process.env.ACCESS_TOKEN_SECRET || 'default-token';
-    jwt.verify(token, secretKey, (err: any, user: any) => {
+    jwt.verify(token, secretKey, (err: TokenExpiredError, user: any) => {
         if (err) {
-            console.log(err);
-            return res.sendStatus(403);
+            return res.json({ message: err.message }).sendStatus(403);
         }
         req.body = user;
-        next()
+        next();
     })
 }
