c46ff70ae9587ab4ceccb003508a4568fe9c6cc1
Gustavo Valle <gustavo.vallerp@hotmail.com>
2021-12-19 21:24:16 -0300

    login and register implementation

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,46 @@
 import jwt from 'jsonwebtoken';
 import { orders } from './data/fake';
-import { Router } from "express";
+import { response, Router } from "express";
 import { authenticateToken } from "./security/authentication";
-
+import { prisma, PrismaClient } from '@prisma/client';
 
 
 const router = Router();
 
-router.post('/users', (request, response) => {
+router.get('/users', authenticateToken, async (request, response) => {
+    const prisma = new PrismaClient();
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            password: false
+        },
+        where: {
+            role: "USER"
+        }
+    })
+
+    return response.json(users).status(200).send();
+})
+
+
+router.post('/register', async (request, response) => {
+    const email = request.body.email;
+    const name = request.body.name;
+    const password = request.body.password;
+
+    const prisma = new PrismaClient();
+
+    await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: password,
+            role: "USER"
+        }
+    })
     return response.status(201).send();
 })
 
@@ -17,18 +50,30 @@ router.get('/', authenticateToken, async (req, res) => {
     res.send(ordersFiltered);
 })
 
-router.post('/login', (req, res) => {
-    const username = req.body.username;
-    const user = { name: username };
+router.post('/login', async (req, res) => {
+    const prisma = new PrismaClient();
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    });
+
+    if (!user || !user.password === password) {
+        return res.status(401).send('email or password incorret(s)')
+    }
 
     const accessToken = generateAccessToken(user);
     res.json({ accessToken: accessToken });
 })
 
 
-const generateAccessToken = (user: any) => {
+const generateAccessToken = (user: Object) => {
     const secretKey = process.env.ACCESS_TOKEN_SECRET || 'default-token';
-    return jwt.sign(user, secretKey, { expiresIn: '15s' });
+    return jwt.sign(user, secretKey, { expiresIn: '86400s' });
 }
 
 
