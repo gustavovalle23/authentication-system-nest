b73c22c3163bc3d6a63503a14770dd27a43ec9fb
Gustavo Valle <gustavo.vallerp@hotmail.com>
2021-12-25 20:48:20 -0300

    remove database consult

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,45 +1,34 @@
 import { Router } from "express";
-import prisma from "../data/prisma";
+import { findUserByEmail, findUserById, findUsersAll, saveUser } from "../repository/userRepository";
 import { authenticateToken, generateAccessToken } from '../security/authentication';
 
 
-const router = Router();
+const router = Router({});
+
+
+router.get('/user/all', authenticateToken, async (request, response) => {
+    const users = await findUsersAll();
+
+    return response.json(users).status(200).send();
+})
+
 
 router.get('/user/:userId', async (request, response) => {
-    console.log('Request to "/user"')
+    console.log('Request to "/user"');
 
     const userId = +request.params.userId;
     if (isNaN(userId)) {
-        return response.status(400).send({ message: 'Id must be a integer!' })
+        return response.status(400).send({ message: 'Id must be a integer!' });
     }
 
-    const user = await prisma.user.findUnique({
-        where: { id: userId }
-    })
+    const user = await findUserById(userId);
 
     if (!user) {
-        return response.status(400).send({ message: 'User not found!' })
+        return response.status(400).send({ message: 'User not found!' });
     }
 
 
     return response.status(200).send(user);
-})
-
-router.get('/user/all', authenticateToken, async (request, response) => {
-    const users = await prisma.user.findMany({
-        select: {
-            id: true,
-            name: true,
-            email: true,
-            role: true,
-            password: false
-        },
-        where: {
-            role: "USER"
-        }
-    })
-
-    return response.json(users).status(200).send();
 })
 
 
@@ -48,22 +37,14 @@ router.post('/user/register', async (request, response) => {
     const name = request.body.name;
     const password = request.body.password;
 
-    const user = await prisma.user.findFirst({
-        where: { email: email }
-    });
+    const user = await findUserByEmail(email);
 
     if (user) {
         return response.status(400).json({ message: 'User already registered!' });
     }
 
-    await prisma.user.create({
-        data: {
-            name: name,
-            email: email,
-            password: password,
-            role: "USER"
-        }
-    })
+    saveUser(name, email, password);
+
     return response.status(201).send();
 })
 
@@ -73,14 +54,10 @@ router.post('/user/authenticate', async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const user = await prisma.user.findUnique({
-        where: {
-            email: email
-        }
-    });
+    const user = await findUserByEmail(email);
 
     if (!user || !user.password === password) {
-        return res.status(401).send({ message: 'email or password incorret(s)' })
+        return res.status(401).send({ message: 'email or password incorret(s)' });
     }
 
     const accessToken = generateAccessToken(user);
