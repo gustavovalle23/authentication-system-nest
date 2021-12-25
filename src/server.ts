5943613460f6b18e7a52ada1cbb0fb49efabf7b2
Gustavo Valle <gustavo.vallerp@hotmail.com>
2021-12-25 11:26:46 -0300

    change host and port to .env

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = 8000;
+const HOST = process.env.DB_HOST;
+const PORT = process.env.DB_PORT;
 
 app.listen(PORT, () => {
-    console.log(`> [server] Server is running at http://localhost:${PORT}`)
+    console.log(`> [server] Server is running at ${HOST}:${PORT}`)
 })
