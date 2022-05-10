a5d8916c25965b6a20d71a44e506703177312e24
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-05-09 23:41:50 -0300

    ajustes iniciais de import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { PrismaService } from 'prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+  ],
+  controllers: [
+    UserController,
+  ],
+  providers: [
+    UserService,
+    PrismaService,
+  ],
 })
 export class AppModule {}
